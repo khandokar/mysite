6dcd9dddaaf3aafc1a64356da1d5f081cfbfb470
khandokar <khandokarsabbir@gmail.com>
2024-01-15 11:06:02 -0700

    Seperate component for about, education, skills, works, blog and contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorksComponent } from './works/works.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AboutComponent, EducationComponent, SkillsComponent, WorksComponent, BlogComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
