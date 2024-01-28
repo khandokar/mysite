2fb0d4a1002c9cf05e6193ddf632405a7a7a0b1c
khandokar <khandokarsabbir@gmail.com>
2024-01-28 16:27:59 -0700

    Remove proxy

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'https://5im0n1cx5b.execute-api.us-east-1.amazonaws.com/V1/contact';
-//const productionUrl = 'https://oqu0fqbl41.execute-api.us-east-1.amazonaws.com/V1/Contact'
+//const baseUrl = 'https://5im0n1cx5b.execute-api.us-east-1.amazonaws.com/V1/contact';
+const productionUrl = 'https://oqu0fqbl41.execute-api.us-east-1.amazonaws.com/V1/Contact'
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,6 @@ export class ContactService {
 
   create(data: any): Observable<any> {
 
-    return this.http.post("/api", data);
+    return this.http.post(productionUrl, data);
   }
 }
